a901c066e29598edd5b094ec8dab673ce26710c0
Purnima Shidore <sagarshidore@gmail.com>
2023-03-15 09:49:36 -0400

    added while loop

--- Program1.java
+++ Program1.java
@@ -6,10 +6,17 @@ public class Program1 {
         int a =10;
         int b=20;
         int c=a+b;
-    for(int i=0;i<s.length();i++){
+    for(int i=0;i<s.length();i++) // for loop
+    {
         temp=s.charAt(i)+temp;
     }
-        StringBuilder sb = new StringBuilder(s);
+    int i=0; // while loop
+    while(i<s.length()) {
+        System.out.println(temp =s.charAt(i)+ temp);
+        i++;
+    }
+
+        StringBuilder sb = new StringBuilder(s); // using java function
         sb.reverse();
         System.out.println(sb);
         System.out.println("Reverse String is : "+temp);
