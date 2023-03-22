c218094dba30879bcca8d2e0587180ad4bc49414
Purnima Shidore <sagarshidore@gmail.com>
2023-03-21 22:25:54 -0400

    encapsulation program added

--- Programs/encapsulation.java
+++ Programs/encapsulation.java
@@ -1,2 +1,24 @@
-package Programs;public class encapsulation {
-}
+package Programs;
+
+public class encapsulation {
+        private int value; // data hiding
+
+        public void setValue(int x) // data abstraction
+        {
+            value = x;
+        }
+        public int getValue() {
+            return value;
+        }
+       class B{
+            public static void main(String[] args){
+                encapsulation r= new encapsulation();
+                r.setValue(100);
+                System.out.println(r.getValue());
+            }
+        }
+
+    }
+
+
+
