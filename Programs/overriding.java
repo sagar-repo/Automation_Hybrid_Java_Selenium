80913357be231ed6a9d3b4e36aacab1e8a64ca85
Purnima Shidore <sagarshidore@gmail.com>
2023-03-21 22:24:48 -0400

    overriding program added

--- Programs/overriding.java
+++ Programs/overriding.java
@@ -1,2 +1,25 @@
-package Programs;public class overriding {
+package Programs;
+
+class overriding {
+    void add(){
+       int a = 10;
+       int b = 20;
+        int c;
+        c=a+b;
+        System.out.println(c);
+    }
+    public static void main(String[] args) {
+        overriding ov= new child();
+                ov.add();
+    }
+}
+
+class child extends overriding{
+    void add(){
+        super.add();
+        int a= 20;
+        int b = 30;
+       int c=a-b;
+        System.out.println(c);
+    }
 }
